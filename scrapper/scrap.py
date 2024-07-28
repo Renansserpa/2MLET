38bfbe583a5531c00b2c337d47752b6cfb2f11b0
Renan Santos Serpa <renans.serpa@gmail.com>
2024-07-27 23:40:50 -0300

    Added docstrings

--- scrapper/scrap.py
+++ scrapper/scrap.py
@@ -16,21 +16,49 @@ abas = {
 }
 
 def http_get(url: str, aba: str, subopcao: Optional[str]  = None, ano: Optional[str] = None) -> BeautifulSoup:
+    """Realiza uma requisição GET no link informado junto de outros parâmetros. Ele retorna um objeto BeautifulSoup com o html obtido.
+
+    Arguments:
+        url {str} -- URL da embrapa que irá receber a requisição
+        aba {str} -- Chave do dicionário "abas". Informe com base em qual aba do site da embrapa gostaria de fazer a requisição.
+        subopcao {Optional[str]} -- Se este parâmetro for informado, adicona o parâmetro "subopcao" na requisição.
+        ano {Optional[str]} -- Se este parâmetro for informado, adicona o parâmetro "ano" na requisição.
+    """
     response = requests.get(url, params= {'opcao': abas[aba], 'subopcao': subopcao, 'ano': ano})
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
 
 def get_available_suboptions(embrapa_url: str, aba: str) -> dict[str,str]:
+    """Realiza requisição GET na url informada e, para a aba da página mencionada, retorna os valores existentes de subabas (subopções).
+    O dicionário retornado contém o nome da subopção conforme seria utilizado na forma de parâmetro durante uma requisição, e o nome como é exibido na página do navegador.
+
+    Arguments:
+        url {str} -- URL da embrapa que irá receber a requisição
+        aba {str} -- Chave do dicionário "abas". Informe com base em qual aba do site da embrapa gostaria de fazer a requisição.
+    """
     soup = http_get(embrapa_url, aba)
     return {f'subopt_0{n +1}': sub_option.text for n, sub_option in enumerate(soup.find_all('button', 'btn_sopt'))}
 
 def get_available_years(embrapa_url: str, aba: str, subopcao: Optional[str]  = None):
+    """Realiza requisição GET na url informada com parâmetros adicionados e retorna a faixa de anos em que há dados disponíveis
+
+    Arguments:
+        embrapa_url {str} -- URL da embrapa que irá receber a requisição
+        aba {str} -- Chave do dicionário "abas". Informe com base em qual aba do site da embrapa gostaria de fazer a requisição.
+        subopcao {Optional[str]} -- Se este parâmetro for informado, adicona o parâmetro "subopcao" na requisição.
+    """
     soup = http_get(embrapa_url, aba, subopcao)
     periodo_busca = soup.find_all('label','lbl_pesq')[0].text
     ano_inicio, ano_fim = re.findall(r'\[(\d{4})-(\d{4})\]', periodo_busca)[0]
     return ano_inicio, ano_fim
 
 def structure_table(soup: BeautifulSoup, table_attr: str) -> pd.DataFrame:
+    """Gera um dataframe com dados da tabela de uma página.
+
+    Arguments:
+        soup {BeautifulSoup} -- Objeto BeautifulSoup gerado a partir de uma requisição e html do site da Embrapa
+        table_attr {str} -- Nome do elemento tabela, escrito no html obtido de uma requisição.
+    """
     linhas_com_valores = []
     
     # Iteração dentro da tabela, para cada linha
@@ -56,6 +84,8 @@ def structure_table(soup: BeautifulSoup, table_attr: str) -> pd.DataFrame:
     
 
 def scrap_producao() -> pd.DataFrame:
+    """ Gera um dataframe com dados de tabelas de todos os anos disponíveis, da aba produção do site da Embrapa.
+    """
     aba = 'producao'
     dfs = []
     ano_inicio, ano_fim = get_available_years(embrapa_url = embrapa_url, aba = aba)
@@ -70,6 +100,8 @@ def scrap_producao() -> pd.DataFrame:
 
 
 def scrap_processamento() -> pd.DataFrame:
+    """ Gera um dataframe com dados de tabelas de todos os anos e subabas disponíveis, da aba processamento do site da Embrapa.
+    """
     aba = 'processamento'
     dfs = []
     
@@ -95,6 +127,8 @@ def scrap_processamento() -> pd.DataFrame:
 
 
 def scrap_comercializacao() -> pd.DataFrame:
+    """ Gera um dataframe com dados de tabelas de todos os anos, da aba comercialização do site da Embrapa.
+    """
     aba = 'comercializacao'
     dfs = []
     ano_inicio, ano_fim = get_available_years(embrapa_url = embrapa_url, aba = aba)
@@ -109,6 +143,8 @@ def scrap_comercializacao() -> pd.DataFrame:
 
 
 def scrap_importacao() -> pd.DataFrame:
+    """ Gera um dataframe com dados de tabelas de todos os anos e subabas disponíveis, da aba importação do site da Embrapa.
+    """
     aba = 'importacao'
     dfs = []
     
@@ -129,6 +165,8 @@ def scrap_importacao() -> pd.DataFrame:
 
 
 def scrap_exportacao() -> pd.DataFrame:
+    """ Gera um dataframe com dados de tabelas de todos os anos e subabas disponíveis, da aba exportação do site da Embrapa.
+    """
     aba = 'exportacao'
     dfs = []
     
